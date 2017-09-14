2d62574d310e7cde5a9ab3ee90aa5e63717cb91f
Ivan Wong <team.iwong@gmail.com>
2017-09-14 16:05:21 +0300

    Complete layout design

--- src/app/containers/Employees.tsx
+++ src/app/containers/Employees.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import { PageHeader } from '../components/layout/PageHeader';
+
 /**
  * Employees page container
  */
 class Employees extends React.PureComponent<{}, {}> {
   public render() {
     return (
-      <div>
-        <h1>employees</h1>
+      <div className="content">
+        <PageHeader title={'Employees management'}/>
       </div>
     );
   }
